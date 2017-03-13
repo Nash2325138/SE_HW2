f18ce7695a8eacf7c43578c080b8a6b530907098
劉哲宇 <j2325138@gmail.com>
2017-03-13 21:51:15 +0800

    Update constructor of Grades; GradeSystem read input file

--- src/gradeSystemProject/GradeSystems.java
+++ src/gradeSystemProject/GradeSystems.java
@@ -1,5 +1,29 @@
 package gradeSystemProject;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.*;
 
 public class GradeSystems {
-
+	private Vector<Grades> gradesCollection;
+	private float[] weights = new float[5]; 
+	public GradeSystems () {
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new FileReader("gradeinput.txt"));
+			String line;
+			while ((line = bufferedReader.readLine()) != null)
+				gradesCollection.add(new Grades(line));
+			bufferedReader.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("File suck");
+		}
+		
+	}
+	public void updateWeights (float[] newWeights) {
+		weights = Arrays.copyOf(newWeights, newWeights.length);
+	}
+	
 }
--- src/gradeSystemProject/Grades.java
+++ src/gradeSystemProject/Grades.java
@@ -3,16 +3,20 @@ import java.util.*;
 
 public class Grades {
 	public String ID, name;
-	public ArrayList<Integer> grades = new ArrayList<>();
-	public Grades(String ID, String name, ArrayList<Integer> grades) {
-		this.ID = ID;
-		this.name = name;
-		this.grades = grades;
+	public Vector<Integer> grades;
+	public Grades(String rawInput) {
+		String[] inputs = rawInput.split(" ");
+		this.ID = inputs[0];
+		this.name = inputs[1];
+		for (int i=2; i<7; i++) {
+			this.grades.add(Integer.parseInt(inputs[i]));
+		}
+		assert(this.grades.size() == 5);
 	}
 	/*
 	 * Calculate the weighted total grade
 	 */
-	public float calculateTotalGrade(ArrayList<Float> weights) {
+	public float calculateTotalGrade(Vector<Float> weights) {
 		float ret = 0;
 		for (int i=0; i<grades.size(); i++) {
 			ret += weights.get(i) * grades.get(i);
