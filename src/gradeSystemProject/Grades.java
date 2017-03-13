afda3b63f83f09b5a734cb8aa40a71932847255d
劉哲宇 <j2325138@gmail.com>
2017-03-13 21:24:31 +0800

    Grades.java implementation

--- src/gradeSystemProject/Grades.java
+++ src/gradeSystemProject/Grades.java
@@ -1,5 +1,22 @@
 package gradeSystemProject;
+import java.util.*;
 
 public class Grades {
-
+	public String ID, name;
+	public ArrayList<Integer> grades = new ArrayList<>();
+	public Grades(String ID, String name, ArrayList<Integer> grades) {
+		this.ID = ID;
+		this.name = name;
+		this.grades = grades;
+	}
+	/*
+	 * Calculate the weighted total grade
+	 */
+	public float calculateTotalGrade(ArrayList<Float> weights) {
+		float ret = 0;
+		for (int i=0; i<grades.size(); i++) {
+			ret += weights.get(i) * grades.get(i);
+		}
+		return ret / 100;
+	}
 }
