47e4d18520ce199120d14e52812b38c7de12e8bd
劉哲宇 <j2325138@gmail.com>
2017-03-16 13:28:54 +0800

    GradeSystemProject spec

--- src/gradeSystemProject/UI.java
+++ src/gradeSystemProject/UI.java
@@ -1,5 +1,34 @@
 package gradeSystemProject;
 
 public class UI {
-
+	 GradeSystems aGradeSystems = new GradeSystems();
+	 String queryingID;
+	 public UI() {
+		 // while( prompt ID, checkID == True);
+		 // while (not exit)
+		 // 	showWelcomeMsg
+		 // 	prompt command
+		 // 		showGrade or showRank or updateWeights
+		 // showFinishMsg
+	 }
+	 boolean checkID(String ID) {
+		 return false;
+	 }
+	 void promptID() {
+		 
+	 }
+	 void showWelcomeMsg() {
+		 
+	 }
+	 void showFinishMsg() {
+		 
+	 }
+	 /*
+	  * prompt command and execute certain function using aGradeSystems
+	  * return false when a 'exit' command is found
+	  */
+	 boolean promptCommand() {
+		return false; 
+	 }
+	 
 }
