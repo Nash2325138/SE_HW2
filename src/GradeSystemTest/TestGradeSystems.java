ff9ea7b144d3373b58c7bdc00ea0f596664300ae
Vincent <chenjuzen@gmail.com>
2017-03-18 20:57:27 +0800

    add comment of TestGradeSystems.java

--- src/GradeSystemTest/TestGradeSystems.java
+++ src/GradeSystemTest/TestGradeSystems.java
@@ -12,42 +12,47 @@ import org.junit.Test;
 
 import gradeSystemProject.GradeSystems;
 
+//Unit Test of GradeSystems class
+
 public class TestGradeSystems {
 	GradeSystems system;
+	
+	//new a GradeSystems class
 	@Before
 	public void setUpBeforeClass() throws Exception {
 		system = new GradeSystems();
 	}
-
+	
 	@After
 	public void tearDownAfterClass() throws Exception {
 		system = null;
 	}
-
+	//test of showRank with correct ID
 	@Test
 	public void testRank() {
 		String poorGuy = "962001051";
 		system.showRank(poorGuy);
 	}
-	
+	//test of showRank with wrong ID
 	@Test 
 	public void testRank2(){
 		String randomID = "990909324";
 		system.showRank(randomID);
 		
 	}
+	//test of showGrade with correct ID
 	@Test
 	public void testShowGrade() {
 		String poorGuy = "962001051";
 		system.showGrade(poorGuy);
 	}
-	
+	//test of showGrade with wrong ID
 	@Test
 	public void testShowGrade2() {
 		String randomID = "135789789";
 		system.showGrade(randomID);
 	}
-	
+	//test of getAverages
 	@Test
 	public void testaverage()
 	{
