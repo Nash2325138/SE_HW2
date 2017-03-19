d7d37c9b96566786742ebb1a988f0a02b97614b0
劉哲宇 <j2325138@gmail.com>
2017-03-19 11:26:39 +0800

    Add comments and split functions into smaller ones

--- src/gradeSystemProject/Main.java
+++ src/gradeSystemProject/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UI ui = new UI();
-		
+		ui.run();
 	}
 }
--- src/gradeSystemProject/UI.java
+++ src/gradeSystemProject/UI.java
@@ -6,17 +6,17 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UI {
-	 GradeSystems aGradeSystems = new GradeSystems();
-	 Scanner scanner = new Scanner(System.in);
+	 GradeSystems aGradeSystems;
+	 Scanner scanner;
 	 String queryingID;
 	 public UI() {
-		 // while( prompt ID )
-		 // 	if ( checkID == False ) continue
-		 // 	while (not exit)
-		 // 		showWelcomeMsg
-		 // 		prompt command
-		 // 			showGrade or showRank or updateWeights
-		 // 	showFinishMsg
+		 aGradeSystems = new GradeSystems();
+		 scanner = new Scanner(System.in);
+	 }
+	 /*
+	  * The method that runs the UI to deal with the scenarios described in spec 
+	  */
+	 public void run() {
 		 while (promptID() == true) {
 			 if (aGradeSystems.containsID(queryingID) == false) {
 				 System.out.println("Sorry, but we don't find ID: " + queryingID);
@@ -26,13 +26,12 @@ public class UI {
 			 while (promptCommand());
 			 showFinishMsg();
 		 }
-		 System.out.println("Thanks for using grade system!!");
+		 System.out.println("Thanks for using grade system!!");		 
 	 }
-	 
 
 	 /*
-	  * prompt ID, when getting a "Q" -> false 
-	  * else -> true
+	  * Prompt ID, when getting a "Q" -> false 
+	  * Else -> true
 	  */
 	 private boolean promptID() {
 		 System.out.println("Please enter your ID or enter \"Q\" or \"q\" to quit");
@@ -44,14 +43,23 @@ public class UI {
 			 return true;
 		 }
 	 }
+	 /*
+	  * Show welcome message with ascii art file
+	  */
 	 private void showWelcomeMsg() {
 		 System.out.println("Welcome, " + aGradeSystems.getName(queryingID));
 		 printTxtFile("welcome.txt");
 	 }
+	 /*
+	  * Show goodbye with ascii art file
+	  */
 	 private void showFinishMsg() {
 		 System.out.println("Goodbye, " + aGradeSystems.getName(queryingID));
 		 printTxtFile("finish.txt");
 	 }
+	 /*
+	  * Print a entire txt file 
+	  */
 	 private void printTxtFile(String txt) {
 		 try (BufferedReader br = new BufferedReader(new FileReader(txt))) {
 			 String line = null;
@@ -65,8 +73,8 @@ public class UI {
 	 }
 	 
 	 /*
-	  * prompt command and execute certain function using aGradeSystems
-	  * return false when a 'exit' command is found
+	  * Prompt command and execute certain function using aGradeSystems.
+	  * Return false when a 'exit' command is found.
 	  */
 	 private boolean promptCommand() {
 		System.out.println("Please enter a command");
@@ -82,6 +90,9 @@ public class UI {
 		executeCommand(command);
 		return true; 
 	 }
+	 /*
+	  * Execute the command read in promptCommand()
+	  */
 	 private void executeCommand(String command) {
 		 if (command.equals("G")) {
 			aGradeSystems.showGrade(queryingID);
@@ -95,6 +106,9 @@ public class UI {
 			System.out.println("No such command");
 		}
 	 }
+	 /*
+	  * Get the average scores from system and show them on screen
+	  */
 	 private void showAverage() {
 		 System.out.println("The average scores are listed below");
 		 String[] examNames = aGradeSystems.getExamNames();
@@ -104,12 +118,27 @@ public class UI {
 		}
 		 System.out.println();
 	 }
+	 /*
+	  * Prompt user to input the weights they want to update
+	  */
 	 private void promptUpdateWeight() {
 		 String[] examNames = aGradeSystems.getExamNames();
 		 float[] new_weights = new float[examNames.length];
-		 float sum = 0;
 		 
 		 System.out.println("Please enter the new weights below (in %)");
+		 float sum = getNewWeights_sumup(examNames, new_weights);
+		 
+		 if (Math.abs(sum-100.) < 0.0001) {
+			 aGradeSystems.updateWeights(new_weights);
+		 } else {
+			 System.err.println("The sum of new weights is not 100%");
+		 }
+	 }
+	 /*
+	  * Get the weights input from user
+	  */
+	 private float getNewWeights_sumup(String[] examNames, float[] new_weights) {
+		 float sum = 0;
 		 for (int i = 0; i < examNames.length; i++) {
 			System.out.print("\t" + examNames[i] + ": ");
 			try {
@@ -122,12 +151,8 @@ public class UI {
 				continue;
 			}
 		 }
+		 // eat one new line
 		 scanner.nextLine();
-		 if (Math.abs(sum-100.) < 0.0001) {
-			 aGradeSystems.updateWeights(new_weights);
-		 } else {
-			 System.err.println("The sum of new weights is not 100%");
-		 }
+		 return sum;
 	 }
-	 
 }
